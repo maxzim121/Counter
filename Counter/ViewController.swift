8ad7f878db722d2606505fce9ce7eb6c3b041ce4
maxzim121 <zimensm12@gmail.com>
2023-02-14 10:07:46 +0400

    Был убран вывод на консоль "Hello world". Созданы и отформатированы кнопка и лейбл. Создана обработка нажатия на кнопку: при нажатии значение счетчика увеличивается.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var counterNumber: Int = 0
 
+    @IBOutlet weak var counterButton: UIButton!
+    
+    
+    @IBAction func touchToCount(_ sender: Any) {
+        counterNumber += 1
+        counterLabel.text = "Значение счетчика: \(counterNumber)"
+    }
+    
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello world!")
+        counterLabel.text = "Значение счетчика: \(counterNumber)"
         // Do any additional setup after loading the view.
     }
 
